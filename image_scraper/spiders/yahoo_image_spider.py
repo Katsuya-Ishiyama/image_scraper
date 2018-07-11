c3c858e2d67ef5b7e00f246ae1021e429bf0bdd1
Katsuya-Ishiyama <ishiyama.katsuya@gmail.com>
2018-07-11 14:21:07 +0900

    add follow link

--- image_scraper/spiders/yahoo_image_spider.py
+++ image_scraper/spiders/yahoo_image_spider.py
@@ -7,6 +7,10 @@ class YahooImageSpider(scrapy.Spider):
 
     name = 'yahoo_image'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.image_id = 0
+
     def start_requests(self):
         url = ('https://search.yahoo.co.jp/image/search?'
                'p={}&ei=UTF-8&save=0'.format(self.query))
@@ -14,13 +18,22 @@ class YahooImageSpider(scrapy.Spider):
 
     def parse(self, response):
         query = self.query
-        img_id = 0
 
         for src in response.css('#gridlist > div'):
-            img_id += 1
-            yield {
-                'id': img_id,
+            self.image_id += 1
+            extracted = {
+                'id': self.image_id,
                 'query': query,
                 'src': src.css('div > p > a > img::attr(src)').extract_first()
             }
+
+            if self.image_id >= 40:
+                return extracted
+            else:
+                yield extracted
+
+        css_next_page = '#Sp1 > p > span.m > a::attr(href)'
+        next_page = response.css(css_next_page).extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
 
