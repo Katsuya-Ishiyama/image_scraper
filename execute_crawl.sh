5a073c0d3c37b6a347e3d63411302035dbeb8277
Katsuya-Ishiyama <ishiyama.katsuya@gmail.com>
2018-07-18 21:58:39 +0900

    execute on member_list

---
+++ execute_crawl.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+csv_file="member_list.csv"
+
+for line in `cat ${csv_file}`
+do
+    member_id=`echo ${line} | cut -d ',' -f 1`
+    member_name=`echo ${line} | cut -d ',' -f 2`
+    echo "processing member_id ${member_id}, member_name ${member_name}"
+    scrapy crawl yahoo_image -a query=$member_name -a member_id=$member_id
+done
+
