fd6a08ab1783fe2c15cce114bb02d6b62f2b606d
Katsuya-Ishiyama <ishiyama.katsuya@gmail.com>
2018-07-19 12:48:25 +0900

    fix to correctly follow links of next page

--- image_scraper/spiders/yahoo_image_spider.py
+++ image_scraper/spiders/yahoo_image_spider.py
@@ -17,6 +17,7 @@ class YahooImageSpider(CrawlSpider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.image_id = 0
+        self.is_first_page = True
 
     def start_requests(self):
         url = ('https://search.yahoo.co.jp/image/search?'
@@ -38,7 +39,13 @@ class YahooImageSpider(CrawlSpider):
 
         yield items
 
-        css_next_page = '#Sp1 > p > span.m > a::attr(href)'
+        # CSS selectors are different between the first page of the result
+        # and other pages.
+        if self.is_first_page:
+            css_next_page = '#Sp1 > p > span.m > a::attr(href)'
+            self.is_first_page = False
+        else:
+            css_next_page = '#Sp1 > p > span:nth-child(13) > a::attr(href)'
         next_page = response.css(css_next_page).extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
