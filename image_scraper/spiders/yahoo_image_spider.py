736f244eff53e293edbb7402d212886da63b5154
Katsuya-Ishiyama <ishiyama.katsuya@gmail.com>
2018-07-19 14:23:09 +0900

    fix next page

--- image_scraper/spiders/yahoo_image_spider.py
+++ image_scraper/spiders/yahoo_image_spider.py
@@ -17,7 +17,8 @@ class YahooImageSpider(CrawlSpider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.image_id = 0
-        self.is_first_page = True
+        self.page_id = 0
+        self.image_num_per_page = 20
 
     def start_requests(self):
         url = ('https://search.yahoo.co.jp/image/search?'
@@ -25,6 +26,8 @@ class YahooImageSpider(CrawlSpider):
         yield scrapy.Request(url, self.parse, dont_filter=True)
 
     def parse(self, response):
+        self.page_id += 1
+
         items = ImageScraperItem()
         items['member_id'] = self.member_id
         items['image_ids'] = []
@@ -39,13 +42,11 @@ class YahooImageSpider(CrawlSpider):
 
         yield items
 
-        # CSS selectors are different between the first page of the result
-        # and other pages.
-        if self.is_first_page:
-            css_next_page = '#Sp1 > p > span.m > a::attr(href)'
-            self.is_first_page = False
-        else:
-            css_next_page = '#Sp1 > p > span:nth-child(13) > a::attr(href)'
-        next_page = response.css(css_next_page).extract_first()
+        _next_page = ('https://search.yahoo.co.jp/image/search?fr=top_ga1_sa&'
+                      'p={query}&ei=UTF-8&b={start_image}')
+        next_page = _next_page.format(
+            query=self.query,
+            start_image=self.page_id * self.image_num_per_page + 1
+        )
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
